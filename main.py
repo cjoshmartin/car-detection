14b12c1a0339044e73b21beadb14a38b705e6775
Josh Martin <joshmartin5001@gmail.com>
2018-09-16 17:56:56 -0400

    images to JSON and imported from JSON

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import glob, os
 
 import jsonpickle
 import jsonpickle.ext.numpy as jsonpickle_numpy
+
 jsonpickle_numpy.register_handlers()
 
-class parseImages:
+
+class ParseImages:
     def __init__(self):
         self.__data = {
             'training': {
@@ -21,37 +23,47 @@ class parseImages:
             }
         }
 
-    def toMatrix(self,pic):
+    def to_matrix(self, pic):
         return array(Image.open(pic))
 
-    def toMatrixArr(self,path, type, label):
+    def to_matrix_arr(self, path, type, label):
         for infile in glob.glob(path):
             file, ext = os.path.splitext(infile)
             dictName = file.split('-')[1]
-            self.__data[type][label][dictName] = self.toMatrix(infile)
+            self.__data[type][label][dictName] = self.to_matrix(infile)
 
-        print('dataset->{}->{} parsed successfully'.format(type,label))
+        print('dataset->{}->{} parsed successfully'.format(type, label))
 
-    def getData(self):
+    def get_data(self):
         return self.__data
 
-    def JSONify(self,path):
+    def JSONify(self, path):
         print('saving to `{}`'.format(path))
         output = jsonpickle.encode(self.__data)
         json.dump(output, codecs.open(path, 'w', encoding='utf-8'))  ### this saves the array in .json format
         print('Done saving')
 
-class handleData:
-    def __init__(self):
-        output = parseImages()
-        output.toMatrixArr('./data_set/TrainImages/pos*.pgm','training','pos')
-        output.toMatrixArr('./data_set/TrainImages/neg*.pgm','training','neg')
-        output.toMatrixArr('./data_set/TestImages/test*.pgm','test','normal')
-        output.toMatrixArr('./data_set/TestImages_Scale/test*.pgm','test','scaled')
-        output.JSONify('./data.json')
+
+def handle_data(save_path):
+    if os.path.isfile(save_path):
+        print('`{}` already exist, I will just load in!'.format(save_path))
+        input_file = codecs.open(save_path, 'r', encoding='utf-8').read()
+        data = jsonpickle.decode(json.loads(input_file))
+    else:
+        print('`{}` does not exist, watch me create it!'.format(save_path))
+        output = ParseImages()
+        output.to_matrix_arr('./data_set/TrainImages/pos*.pgm', 'training', 'pos')
+        output.to_matrix_arr('./data_set/TrainImages/neg*.pgm', 'training', 'neg')
+        output.to_matrix_arr('./data_set/TestImages/test*.pgm', 'test', 'normal')
+        output.to_matrix_arr('./data_set/TestImages_Scale/test*.pgm', 'test', 'scaled')
+        output.JSONify(save_path)
+        data = output.get_data()
+
+    return data
+
 
 def main():
-    data = handleData()
+    data = handle_data('./data.json')
 
 
 main()
