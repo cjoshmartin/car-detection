a4ee0dfcc5f6cd38f8e6a226976de67dfae08c95
Josh Martin <joshmartin5001@gmail.com>
2018-10-05 07:34:04 -0400

    Neuron class has been assambled

--- main.py
+++ main.py
@@ -1,100 +1,20 @@
-from utils.parse_images import handle_data, save_image
+from neuron.neuron import relu, pooling, Convolution, Neuron
+from utils.parse_images import handle_data
 from pylab import *
+import numpy
+import matplotlib.pyplot as plt
 
 
-def demations(tuple1, filter, size=0):
-    d1 = tuple1[0] - filter[1] + 1
-    d2 = tuple1[1] - filter[1] + 1
-
-    return tuple((d1, d2, size))
-
-
-def print_error(msg):
-    print(msg)
-    sys.exit()
-
-
-def conv_(img, conv_filter):
-    filter_size = conv_filter.shape[1]
-    result = zeros((img.shape))
-    # Looping through the image to apply the convolution operation.
-    for r in uint16(arange(filter_size / 2.0,
-                           img.shape[0] - filter_size / 2.0 + 1)):
-        for c in uint16(arange(filter_size / 2.0,
-                               img.shape[1] - filter_size / 2.0 + 1)):
-            """
-            Getting the current region to get multiplied with the filter.
-            How to loop through the image and get the region based on 
-            the image and filer sizes is the most tricky part of convolution.
-            """
-            curr_region = img[r - uint16(floor(filter_size / 2.0)):r + uint16(
-                ceil(filter_size / 2.0)),
-                          c - uint16(floor(filter_size / 2.0)):c + uint16(
-                              ceil(filter_size / 2.0))]
-            # Element-wise multipliplication between the current region and the filter.
-            curr_result = curr_region * conv_filter
-            conv_sum = sum(curr_result)  # Summing the result of multiplication.
-            result[r, c] = conv_sum  # Saving the summation in the convolution layer feature map.
-
-    # Clipping the outliers of the result matrix.
-    final_result = result[uint16(filter_size / 2.0):result.shape[0] - uint16(filter_size / 2.0),
-                   uint16(filter_size / 2.0):result.shape[1] - uint16(filter_size / 2.0)]
-    return final_result
-
-
-def conv(img, conv_filter):
-    if len(img.shape) > 2 or len(conv_filter.shape) > 3:
-        if img.shape[-1] != conv_filter.shape[-1]:
-            print_error('Error: Number of channels in both image and filter must match.')
-        if conv_filter.shape[1] != conv_filter.shape[2]:
-            print_error('Error: Filter must be a square matrix. I.e. number of rows and columns must match.')
-        if conv_filter.shape[2] % 2 == 0:
-            print_error('Error: Filter must have an odd size. I.e. number of rows and columns must be odd.')
-
-    number_of_filters = conv_filter.shape[0]
-    feature_maps = zeros(demations(img.shape, conv_filter.shape, number_of_filters))
-
-    for filter_num in range(number_of_filters):
-        print("Filter ", filter_num + 1)
-        curr_filter = conv_filter[filter_num, :]  # getting a filter from the bank.
-
-        if len(curr_filter.shape) > 2:
-            conv_map = conv_(img[:, :, 0], curr_filter[:, :, 0])
-            for channel_number in range(1, curr_filter.shape[
-                -1]):  # Convolving each channel with the image and summing the results.
-                conv_map = conv_map + conv_(img[:, :, channel_number], curr_filter[:, :, channel_number])
-        else:
-            conv_map = conv_(img, curr_filter)  # single channel in filter
-            feature_maps[:, :, filter_num] = conv_map
-
-    return feature_maps
-
+# def create_filter
 
 def main():
     data = handle_data('./data.json')
     training_data = data['training']
     test_data = data['test']
 
-
-    test_filters = zeros((2, 3, 3))
-
-    test_filters[0, :, :] = np.array([
-        [1, 0, -1],
-        [1, 0, -1],  # vertical edges
-        [1, 0, -1]
-    ])
-
-    test_filters[1, :, :] = np.array([
-        [1, 1, 1],
-        [0, 0, 0],  # Horizontal edges
-        [-1, -1, -1]
-    ])
-
     sample = training_data['pos']['0']
-
-    feature_map = conv(sample, test_filters)
-
-    save_image(feature_map)
+    nuron = Neuron(sample, numpy.max, 'Ly1')
+    nuron.plot_maps(3, 2)
 
 
 main()
--- neuron/neuron.py
+++ neuron/neuron.py
@@ -1,18 +1,125 @@
-
 # layers
+import numpy
+from numpy import uint16, sum
+from numpy.core.multiarray import zeros, arange
+from numpy.core.umath import floor, ceil
+import matplotlib.pyplot as plt
+
+from utils.general import demations, print_error
+from utils.ploting import graphs, plot, save_plot
+
 class Convolution:
-    def __init__(self):
-        self.idk = 'yet'
+    def __init__(self, img, conv_filter):
+        if len(img.shape) > 2 or len(conv_filter.shape) > 3:
+            if img.shape[-1] != conv_filter.shape[-1]:
+                print_error('Error: Number of channels in both image and filter must match.')
+            if conv_filter.shape[1] != conv_filter.shape[2]:
+                print_error('Error: Filter must be a square matrix. I.e. number of rows and columns must match.')
+            if conv_filter.shape[2] % 2 == 0:
+                print_error('Error: Filter must have an odd size. I.e. number of rows and columns must be odd.')
 
-class Pooling:
-    def __init__(self):
-        self.idk = 'yet'
+        number_of_filters = conv_filter.shape[0]
+        self.feature_maps = zeros(demations(img.shape, conv_filter.shape, number_of_filters))
 
-class Fully_Connected:
-    def __init__(self):
-        self.idk = 'yet'
+        for filter_num in range(number_of_filters):
+            print("Filter ", filter_num + 1)
+            curr_filter = conv_filter[filter_num, :]  # getting a filter from the bank.
+
+            if len(curr_filter.shape) > 2:
+                conv_map = self.__conv(img[:, :, 0], curr_filter[:, :, 0])
+                for channel_number in range(1, curr_filter.shape[
+                    -1]):  # Convolving each channel with the image and summing the results.
+                    conv_map = conv_map + self.__conv(img[:, :, channel_number], curr_filter[:, :, channel_number])
+            else:
+                conv_map = self.__conv(img, curr_filter)  # single channel in filter
+                self.feature_maps[:, :, filter_num] = conv_map
+
+    def __conv(self, img, conv_filter):
+        filter_size = conv_filter.shape[1]
+        result = zeros((img.shape))
+        # Looping through the image to apply the convolution operation.
+        size___ = filter_size / 2.0
+        for r in uint16(arange(size___,
+                               img.shape[0] - size___ + 1)):
+            for c in uint16(arange(size___,
+                                   img.shape[1] - size___ + 1)):
+                """
+                Getting the current region to get multiplied with the filter.
+                How to loop through the image and get the region based on 
+                the image and filer sizes is the most tricky part of convolution.
+                """
+                curr_region = img[r - uint16(floor(size___)):r + uint16(
+                    ceil(size___)),
+                              c - uint16(floor(size___)): c + uint16(
+                                  ceil(size___))]
+                # Element-wise multipliplication between the current region and the filter.
+                curr_result = curr_region * conv_filter
+                conv_sum = sum(curr_result)  # Summing the result of multiplication.
+                result[r, c] = conv_sum  # Saving the summation in the convolution layer feature map.
+
+        # Clipping the outliers of the result matrix.
+        final_result = result[uint16(size___): result.shape[0] - uint16(size___),
+                       uint16(size___): result.shape[1] - uint16(size___)]
+        return final_result
+
+
+def relu(feature_map, activation):  # Activation function, normalize of what is passed from the convolution stage
+    output = zeros(feature_map.shape)
+
+    for map_num in range(feature_map.shape[-1]):
+        for r in arange(0, feature_map.shape[0]):
+            for c in arange(0, feature_map.shape[1]):
+                output[r, c, map_num] = activation([feature_map[r, c, map_num], 0])
+
+    return output
+
+
+def pooling(feature_map, activation, size=2, stride=2):
+    # Preparing the output of the pooling operation.
+    pool_out = zeros((uint16((feature_map.shape[0] - size + 1) / stride + 1),
+                      uint16((feature_map.shape[1] - size + 1) / stride + 1),
+                      feature_map.shape[-1]))
+    for map_num in range(feature_map.shape[-1]):
+        r2 = 0
+        for r in arange(0, feature_map.shape[0] - size + 1, stride):
+            c2 = 0
+            for c in arange(0, feature_map.shape[1] - size + 1, stride):
+                pool_out[r2, c2, map_num] = activation([feature_map[r:r + size, c:c + size]])
+                c2 = c2 + 1
+            r2 = r2 + 1
+    return pool_out
+
 
 # Neuron
 class Neuron:
-    def __init__(self):
-        self.idk = None
+    def __init__(self, source, activation, layer_name, filter=None):
+
+        self.layer_name = layer_name
+
+        if filter is None:
+            # input layer
+
+            __fig, __axes = plt.subplots(nrows=1, ncols=1)
+            plot(__axes, source, 'Input')
+            save_plot(__fig, 'input')
+
+            self.filter = numpy.zeros((2, 3, 3))
+            self.filter[0, :, :] = numpy.array([[[-1, 0, 1],
+                                                 [-1, 0, 1],
+                                                 [-1, 0, 1]]])
+            self.filter[1, :, :] = numpy.array([[[1, 1, 1],
+                                                 [0, 0, 0],
+                                                 [-1, -1, -1]]])
+        else:
+            self.filter = filter
+
+        self.feature_maps = Convolution(source, self.filter).feature_maps
+        self.activated_maps = relu(self.feature_maps, activation)
+        self.reduced_maps = pooling(self.activated_maps, activation)
+
+    def plot_maps(self, rows, cols):
+        fig, axes = plt.subplots(nrows=rows, ncols=cols)
+        graphs(axes[0], self.feature_maps, '{}-Map{}', self.layer_name)
+        graphs(axes[1], self.activated_maps, '{}-Map{}ReLU', self.layer_name)
+        graphs(axes[2], self.reduced_maps, '{}-Map{}ReLUPool', self.layer_name)
+        save_plot(fig, self.layer_name)
