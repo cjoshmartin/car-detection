393af0bfa8fef2f213ed84dbe5c6e201d03f38e1
Josh Martin <contact@cjoshmartin.com>
2018-10-14 18:01:30 -0400

    Miss spelled neuron

--- main.py
+++ main.py
@@ -13,29 +13,29 @@ def main():
     activation = relu
 
     sample = training_data['pos']['0']
-    prev_nuron = Neuron(sample, activation, 'Ly1')
-    prev_nuron.activate()
-    prev_nuron.plot_maps(3, prev_nuron.feature_maps.shape[2])
+    prev_neuron = Neuron(sample, activation, 'Ly1')
+    prev_neuron.activate()
+    prev_neuron.plot_maps(3, prev_neuron.feature_maps.shape[2])
 
     meh = [[3,5,5], [1,7,7], [1,1,1]]
 
     for i in range(1, 2):
-        nuron = Neuron(
-            prev_nuron.reduced_maps,
+        neuron = Neuron(
+            prev_neuron.reduced_maps,
             activation,
             'Ly{}'.format(i + 1),
             numpy.random.rand(
                 meh[i-1][0],
                 meh[i-1][1],
                 meh[i-1][2],
-                prev_nuron.reduced_maps.shape[-1]
+                prev_neuron.reduced_maps.shape[-1]
             )
         )
 
-        nuron.activate()
-        nuron.plot_maps(3, nuron.feature_maps.shape[2])
+        neuron.activate()
+        neuron.plot_maps(3, neuron.feature_maps.shape[2])
 
-        prev_nuron = nuron
-    nuron
+        prev_neuron = neuron
+    neuron
 
 main()
