782378c7c6397baea69560c8e3edab7fe0b195ec
Josh Martin <contact@cjoshmartin.com>
2018-10-16 12:28:57 -0400

    Made A Network class
    Saw that a lot that was happening in the main could be in its own class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from network.network import Network
 from neuron.neuron import Neuron, relu
 from utils.parse_images import handle_data
 import numpy
@@ -9,54 +10,18 @@ def main():
     data = handle_data('./data.json')
     training_data = data['training']
     test_data = data['test']
-
+    sample = [training_data['pos'], training_data['neg']]
     activation = relu
 
-    sample = [ training_data['pos'], training_data['neg']]
-    prev_neuron = Neuron(training_data['pos']['0'], activation, 'Ly1')
-    prev_neuron.activate()
-    network = [prev_neuron]
+    layer_config = [[3,5,5], [1,7,7], [1,1,1]]
 
-    meh = [[3,5,5], [1,7,7], [1,1,1]]
-
-    for i in range(len(meh)):
-        neuron = Neuron(
-            prev_neuron.get_map(),
-            activation,
-            'Ly{}'.format(i + 1),
-            numpy.random.rand(
-                meh[i][0],
-                meh[i][1],
-                meh[i][2],
-                prev_neuron.get_map().shape[-1] # TODO: find a way to decomple this, cause to have to run activation function to have shape
-            )
-        )
-        # neuron.set_should_pooling(False)
-        neuron.activate()
-        network.append(neuron)
-
-        prev_neuron = neuron
+    cnn_network = Network(activation, training_data['pos']['0'], layer_config)
 
     epoch = 0
+
     while True:
         print('\nEpoch #{}\n'.format(epoch))
+        cnn_network.train(sample, epoch, 10)
+        epoch += 1
 
-        for i in range(len(sample[1])):
-
-            has_a_car = (i + epoch ) % 2
-            sample = sample[has_a_car]['{}'.format(i)]
-
-            for j in range(len(network)):
-                if j == 0:
-                    network[j].set_image(sample)
-                else:
-                    network[j].set_image(network[j-1].get_map())
-
-                network[j].activate()
-
-                network[j].plot_maps(i)
-
-            if i == 10:
-                break
-        epoch = epoch + 1
 main()
---
+++ network/network.py
@@ -0,0 +1,53 @@
+from neuron.neuron import Neuron
+import numpy
+
+
+class Network:
+
+    def __init__(self, activation, sample_image, layer_configuration, should_pool=True):
+
+        self.__prev_neuron = Neuron(sample_image, activation, 'Ly1')
+        self.__prev_neuron.set_should_pooling(True)
+        self.__prev_neuron.activate()
+        self.__network = [self.__prev_neuron]
+
+        for i in range(len(layer_configuration)):
+            neuron = Neuron(
+                self.__prev_neuron.get_map(),
+                activation,
+                'Ly{}'.format(i + 1),
+                numpy.random.rand(  # creates random values for the filters
+                    layer_configuration[i][0],
+                    layer_configuration[i][1],
+                    layer_configuration[i][2],
+                    self.__prev_neuron.get_map().shape[-1]
+                    # TODO: find a way to decomple this, cause to have to run activation function to have shape
+                )
+            )
+            neuron.set_should_pooling(should_pool)
+            neuron.activate()
+            self.__network.append(neuron)
+
+            self.__prev_neuron = neuron
+
+    def train(self, input_data, cur_epoch, batch_size):
+        for i in range(len(input_data[1])):
+
+            has_a_car = (i + cur_epoch) % 2
+            data = input_data[has_a_car]['{}'.format(i)]
+
+            for j in range(len(self.__network)):
+                if j == 0:
+                    self.__network[j].set_image(data)
+                else:
+                    self.__network[j].set_image(self.__network[j - 1].get_map())
+
+                self.__network[j].activate()
+
+                self.__network[j].plot_maps(i)
+
+            if i == batch_size:
+                break
+
+    def test(self):  # TODO
+        pass
