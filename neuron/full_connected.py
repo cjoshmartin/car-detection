b4f591cd6b8318ec5e1589185fe0528b9ca0ea0f
Josh Martin <contact@cjoshmartin.com>
2018-12-05 04:16:02 -0500

    end of world

--- neuron/full_connected.py
+++ neuron/full_connected.py
@@ -1,6 +1,6 @@
 import numpy
 
-from neuron.convolution import dot_product
+from neuron.convolution import dot_product, conv_
 from neuron.neuron import Neuron
 import utils.activations as activations
 import utils.general as general
@@ -31,12 +31,46 @@ class FullConnected(Neuron):
                                    self.filter.shape[0])
         feature_maps = numpy.zeros(d_out)
         k = 0
-        for i in range(self.filter.shape[-1]):
+        number_of_channels = self.filter.shape[-1]
+        for i in range(number_of_channels):
             curr_channel = self.filter[:, :, :, i]
             for j in range(curr_channel.shape[-1]):
                 feature_maps[:, :, k] = dot_product(last_layer[:, :, j], curr_channel[:, :, j]) \
                                         # + feature_maps[:, :, k]
-                k += 1
+                if k < number_of_channels:
+                    k += 1
+                else:
+                    k = 0
 
         return feature_maps
+
+    # def conv(self, bias):
+    #     img = self._Neuron__source
+    #     conv_filter = self.filter
+    #     # def conv(self, bias):  # function overloading
+    #     # An empty feature map to hold the output of convolving the filter(s) with the image.
+    #     the_shape = (1,
+    #                  1,
+    #                  conv_filter.shape[2])
+    #     feature_maps = numpy.zeros(the_shape)
+    #
+    #     # Convolving the image by the filter(s).
+    #     for filter_num in range(conv_filter.shape[0]):
+    #         print("Filter ", filter_num + 1)
+    #         curr_filter = conv_filter[filter_num, :]  # getting a filter from the bank.
+    #         """
+    #         Checking if there are mutliple channels for the single filter.
+    #         If so, then each channel will convolve the image.
+    #         The result of all convolutions are summed to return a single feature map.
+    #         """
+    #         if len(curr_filter.shape) > 2:
+    #             conv_map = conv_(img[:, :, 0], curr_filter[:, :, 0])  # Array holding the sum of all feature maps.
+    #             for ch_num in range(1, curr_filter.shape[
+    #                 -1]):  # Convolving each channel with the image and summing the results.
+    #                 conv_map = conv_map + conv_(img[:, :, ch_num],
+    #                                             curr_filter[:, :, ch_num])
+    #         else:  # There is just a single channel in the filter.
+    #             conv_map = conv_(img, curr_filter)
+    #         feature_maps[:, :, filter_num] = conv_map  # Holding feature map with the current filter.
+    #     return feature_maps  # Returning all feature maps.
 
